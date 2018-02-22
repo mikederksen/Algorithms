5cc504bdcb4b5af24154e4b59efff826f7f42d78
Mike Derksen <m.derksen@avisi.nl>
2018-02-22 14:15:55 +0100

    Added templates for linkedlist

--- src/main/java/CustomTypes/HANLinkedList.java
+++ src/main/java/CustomTypes/HANLinkedList.java
@@ -1,4 +1,24 @@
 package CustomTypes;
 
-public class HANLinkedList {
+public class HANLinkedList<T> {
+
+    public void addFirst(T value) {
+
+    }
+
+    public void removeFirst() {
+
+    }
+
+    public void insert(int index, T value) {
+
+    }
+
+    public void delete(int index) {
+
+    }
+
+    public void get(int index) {
+
+    }
 }
---
+++ src/test/java/CustomTypes/HANLinkedListTests.java
@@ -0,0 +1,39 @@
+package CustomTypes;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HANLinkedListTests {
+
+    private HANLinkedList sut;
+
+    @Before
+    public void setUp() {
+        sut = new HANLinkedList();
+    }
+
+    @Test
+    public void addFirstAddsNodeInBeginningOfList() {
+
+    }
+
+    @Test
+    public void removeFirstRemovesFirstNode() {
+
+    }
+
+    @Test
+    public void insertInsertsItemAtIndex() {
+
+    }
+
+    @Test
+    public void deleteDeletsItemAtIndex() {
+
+    }
+
+    @Test
+    public void getRetrievesItemAtIndex() {
+
+    }
+}
