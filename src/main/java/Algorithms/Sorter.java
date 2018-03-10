d1cf718451c1d01f0ccef02ba1f0f8e70d7b2798
Mike Derksen <mikederksen5@live.nl>
2018-03-10 22:39:34 +0100

    Added partial mergesort implementation

--- src/main/java/Algorithms/Sorter.java
+++ src/main/java/Algorithms/Sorter.java
@@ -85,6 +85,56 @@ public class Sorter {
         return sortedItems;
     }
 
+
+
+
+
+    private static <T extends Comparable<T>> void mergesort(List<T> input, int low, int high) {
+        if (low < high) {
+            int middle = low + (high - low) / 2;
+
+            mergesort(input, low, middle);
+            mergesort(input, middle + 1, high);
+            merge(input, low, middle, high);
+        }
+    }
+
+    private static <T extends Comparable<T>> void merge(List<T> input, int low, int middle, int high) {
+
+        // Copy both parts into the helper array
+        for (int i = low; i <= high; i++) {
+            helper[i] = numbers[i];
+        }
+
+        int i = low;
+        int j = middle + 1;
+        int k = low;
+        // Copy the smallest values from either the left or the right side back
+        // to the original array
+        while (i <= middle && j <= high) {
+            if (helper[i] <= helper[j]) {
+                numbers[k] = helper[i];
+                i++;
+            } else {
+                numbers[k] = helper[j];
+                j++;
+            }
+            k++;
+        }
+        // Copy the rest of the left side of the array into the target array
+        while (i <= middle) {
+            numbers[k] = helper[i];
+            k++;
+            i++;
+        }
+        // Since we are sorting in-place any leftover elements from the right side
+        // are already at the right position.
+
+    }
+
+
+
+
     public static <T extends Comparable<T>> List quickSort(List<T> input) {
         if (input == null) {
             throw new IllegalArgumentException(INPUT_NOT_NULL_EXCEPTION);
@@ -95,6 +145,19 @@ public class Sorter {
 
         return sortedItems;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private static <T extends Comparable<T>> void quickSort(List<T> arr, int low, int high) {
         if (arr == null || arr.size() == 0) {
